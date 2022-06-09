41cae0e95d63d7d84f87a7e2d61840a490678dca
aliakseis <aliaksei.sanko@gmail.com>
2022-06-09 17:17:01 +0300

    Initial commit

---
+++ SimpleCapture/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется SimpleCapture.rc
+//
+#define IDR_MENU1                       101
+#define IDD_CHOOSE_DEVICE               102
+#define IDC_LIST1                       1001
+#define IDC_DEVICE_LIST                 1001
+#define ID_FILE_CHOOSEDEVICE            40001
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
